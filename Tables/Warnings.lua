4bfbb2038afa5465629daa9bafe664a3ad5fce3f
Axrtas <93473789+axrtas@users.noreply.github.com>
2025-03-31 14:39:45 +0200

    Update Warnings.lua

--- Tables/Warnings.lua
+++ Tables/Warnings.lua
@@ -3,12 +3,9 @@ local WarningsModule = require(game:GetService("ServerScriptService"):FindFirstC
 function WarningsModule.addWarning(player, reason)
     local playerWarnings = WarningsModule.getWarnings(player)
     
-    -- Increment the warning count
     playerWarnings.count = playerWarnings.count + 1
     
-    -- Add the new reason to the list of reasons
     table.insert(playerWarnings.reasons, reason)
 
-    -- Store the updated data back to the DataStore
     WarningsModule.setWarnings(player, playerWarnings)
 end
